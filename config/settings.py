d025cfd6ad7fd29bdb086d3c55ddec923964c17a
DevMrMohammad <mohammadsalarkarimabadi909@gmail.com>
2026-02-18 15:15:58 +0330

    create folder static and pase static file to static folder / crate app PAGES and edit setting : add static code with file / add index.html in templates / edit setting and write app in isntalled app / and write templates in temlplates

--- config/settings.py
+++ config/settings.py
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # my apps
+    'pages.apps.PagesConfig',
 ]
 
 MIDDLEWARE = [
@@ -63,7 +65,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],  # مسیر فولدر templates
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,3 +126,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']  # مسیر فولدر static
+
