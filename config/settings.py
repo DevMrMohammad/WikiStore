a08717f41a79960272eb112b8cfa9a30842357a9
DevMrMohammad <mohammadsalarkarimabadi909@gmail.com>
2026-02-18 16:28:25 +0330

    remove more folder and data for static and front project

--- config/settings.py
+++ config/settings.py
@@ -46,8 +46,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # my apps
-    'pages.apps.PagesConfig',
 ]
 
 MIDDLEWARE = [
@@ -65,7 +63,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],  # مسیر فولدر templates
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,5 +124,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']  # مسیر فولدر static
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
